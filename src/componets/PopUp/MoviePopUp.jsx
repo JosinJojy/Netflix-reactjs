e3efcb2124ead8ca6369e69ed666f2dc328e43b5
JosinJojy <88649531+JosinJojy@users.noreply.github.com>
2023-04-17 10:43:27 +0530

    Update MoviePopUp.jsx

--- src/componets/PopUp/MoviePopUp.jsx
+++ src/componets/PopUp/MoviePopUp.jsx
@@ -23,11 +23,11 @@ function MoviePopUp(props) {
   useEffect(() => {
     setPopupInfo(props.data1);
 
-    if (props.from === "MyList") {
-      removeButtonText = "";
-    } else if (props.from === "WatchedMovies") {
-      removeButtonText = "MyList";
-    }
+    //if (props.from === "MyList") {
+      //removeButtonText = "";
+    //} else if (props.from === "WatchedMovies") {
+      //removeButtonText = "MyList";
+    //}
   }, []);
 
   return (
